6d01d3a86dd12650fc96d527bd1fe9eb7705186e
Ezequiel Barreto <ezequiel.barreto@betechcapital.com>
2020-06-02 18:43:15 -0500

    Project main files

---
+++ TestingIOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestingIOS
+//
+//  Created by beTech CAPITAL on 02/06/20.
+//  Copyright © 2020 Ezequiel Barreto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
