7c58a856794ce1b35e7ee6f127ea8c234621d96c
Ezequiel Barreto <ezequielbrrt@gmail.com>
2020-06-04 13:03:31 -0500

    Bank App testing

---
+++ TestingIOS/Models/Account.swift
@@ -0,0 +1,9 @@
+//
+//  Account.swift
+//  TestingIOS
+//
+//  Created by beTech CAPITAL on 04/06/20.
+//  Copyright © 2020 Ezequiel Barreto. All rights reserved.
+//
+
+import Foundation
---
+++ TestingIOSTests/BankAppTests.swift
@@ -0,0 +1,14 @@
+//
+//  TestingIOSTests.swift
+//  TestingIOSTests
+//
+//  Created by beTech CAPITAL on 03/06/20.
+//  Copyright © 2020 Ezequiel Barreto. All rights reserved.
+//
+
+import XCTest
+
+class TestingIOSTests: XCTestCase {
+
+
+}
